b3a442fb68ebd01c12e74512932cd83e2e818c3d
Lucas Santos <lhs.santoss@gmail.com>
2019-11-21 20:43:51 -0300

    Fix overall try condition

--- src/services/RenderService.ts
+++ src/services/RenderService.ts
@@ -9,12 +9,12 @@ export class RenderService {
   constructor (@inject('Renderers') private readonly renderers: Renderers) { }
 
   async render (lang: string, text: string, data: any) {
-    try {
-      const renderer = this.renderers[lang]
-      if (!renderer) throw new RendererNotFoundError(lang)
-      // tslint:disable-next-line: strict-type-predicates
-      if (typeof renderer !== 'function') throw new InvalidRendererError(lang)
+    const renderer = this.renderers[lang]
+    if (!renderer) throw new RendererNotFoundError(lang)
+    // tslint:disable-next-line: strict-type-predicates
+    if (typeof renderer !== 'function') throw new InvalidRendererError(lang)
 
+    try {
       const templateText = await renderer(text, data)
       return templateText
     } catch (err) {
