de2d121f0f17d724c38c24d76ec0864ba29dfb11
Lucas Santos <lhs.santoss@gmail.com>
2019-12-04 21:05:43 -0300

    Add new Env

--- src/app-config.ts
+++ src/app-config.ts
@@ -5,6 +5,7 @@ import { IExpressoConfigOptions } from '@expresso/app'
 // Your configuration keys goes here
 export interface IAppConfig extends IExpressoConfigOptions {
   defaultFromAddress: string,
+  defaultFromName: string,
   rendererList: string,
   sendgrid: {
     apiKey: string
@@ -16,6 +17,7 @@ export interface IAppConfig extends IExpressoConfigOptions {
 export const config: IAppConfig = {
   name: 'zaqar',
   defaultFromAddress: env.get('DEFAULT_FROM_ADDRESS', 'some@email.com'),
+  defaultFromName: env.get('DEFAULT_FROM_NAME', 'Some Name'),
   rendererList: env.get('RENDERER_LIST', 'zaqar-renderer-ejs zaqar-renderer-mustache'),
   server: {
     printOnListening: true
